bf07d0f277f8e4335b479cd6da372c0babef1294
maksymkpr <maksym.kupriichuk@gmail.com>
2019-09-30 14:58:57 +0300

    configure purify for autodetected cssfile

--- config/helpers/purify.js
+++ config/helpers/purify.js
@@ -1,13 +1,38 @@
 const purify = require('purify-css');
-const cssFile = './dist/css/bundle.8dcd12f.css'; // YOUR CSS NAME
+const fs = require('fs');
+
+
+let cssFile = getFiles('./dist/css');
+let whiteListClass = [
+  'is-open',
+  'dropdown',
+  'active',
+  '*owl*',
+  '*mfp*'
+];
+
 
 const content = ['./dist/js/*.js', './dist/*.html'];
 const css = [cssFile];
 const options = {
 	output: cssFile,
-	whitelist: ['is-open', 'dropdown', 'active', '*owl*', '*mfp*'],
+	whitelist: whiteListClass,
 	info: true,
 	minify: true
 };
+
+
+function getFiles(dir, files_) {
+  files_ = files_ || [];
+  let files = fs.readdirSync(dir);
+  for (let i in files) {
+    let name = dir + '/' + files[i];
+    if (~name.indexOf('.css')) {
+      files_.push(name);
+    }
+  }
+  return files_.join();
+}
+
 
 purify(content, css, options);
