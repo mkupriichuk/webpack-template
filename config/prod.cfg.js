45a50108b0440fe8d054e7d70d457a0300878a84
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2020-03-14 17:03:06 +0200

    pngquant quality > stroke to array

--- config/prod.cfg.js
+++ config/prod.cfg.js
@@ -159,7 +159,7 @@ module.exports = merge(baseConfig, {
                 optimizationLevel: 7
               },
               pngquant: {
-                quality: '65-90',
+                quality: [0.65, 0.90],
                 speed: 4
               },
               mozjpeg: {
