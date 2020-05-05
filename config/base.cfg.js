ef153e3d6f36a4d7cf2049fa695569fae14d4215
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2020-05-05 11:02:59 +0300

    destructing path and fs

--- config/base.cfg.js
+++ config/base.cfg.js
@@ -1,7 +1,7 @@
-const path = require('path');
+const { join, resolve } = require('path');
 const PATHS = {
-	src: path.join(__dirname, '../src'),
-	dist: path.join(__dirname, '../dist')
+	src: join(__dirname, '../src'),
+	dist: join(__dirname, '../dist')
 };
 const nunjucksLoaderOptions = JSON.stringify({
 	searchPaths: [
@@ -14,7 +14,7 @@ const fileLoader = (outputPath, name, ext) => {
 	const loaders = [{
 		loader: 'file-loader',
 		options: {
-      context: path.resolve(__dirname, '../src/'),
+      context: resolve(__dirname, '../src/'),
 			name: name,
 			outputPath: outputPath ? outputPath : undefined
 		}
@@ -38,9 +38,9 @@ module.exports = {
 	},
 	resolve: {
 		alias: {
-			'src': path.resolve(__dirname, '../src'),
-			'icons': path.resolve(__dirname, '../src/images/icons'),
-			'images': path.resolve(__dirname, '../src/images')
+			'src': resolve(__dirname, '../src'),
+			'icons': resolve(__dirname, '../src/images/icons'),
+			'images': resolve(__dirname, '../src/images')
 		}
 	},
 	module: {
@@ -58,7 +58,7 @@ module.exports = {
 			},
 			{
 				test: /\.svg$/,
-				// exclude: path.resolve(__dirname, '../src/images/icons/'),
+				// exclude: resolve(__dirname, '../src/images/icons/'),
 				use: fileLoader('images/', '[name].[ext]', 'svg-transform-loader')
 			}
 		]
--- config/dev.cfg.js
+++ config/dev.cfg.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+const { join, resolve} = require('path');
+const { readdirSync } = require('fs');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,14 +10,13 @@ const baseConfig = require('./base.cfg');
 const stylelintCfg = require('../.stylelintrc');
 
 
-const PAGES = fs
-  .readdirSync('src/')
+const PAGES = readdirSync('src/')
   .filter(fileName => fileName.endsWith(".html"))
   .map(
     page =>
       new HtmlWebpackPlugin({
         filename: `${page}`,
-        template: path.join(__dirname, `../src/${page}`),
+        template: join(__dirname, `../src/${page}`),
         inject: true
       })
   );
@@ -27,7 +26,7 @@ const imagesLoader = () => {
     {
       loader: 'file-loader',
       options: {
-        context: path.resolve(__dirname, '../src/'),
+        context: resolve(__dirname, '../src/'),
         name: 'images/[name].[hash:7].[ext]'
       }
     }
