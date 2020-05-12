a1b2a0ce132e4ce0f672805a3f17a2e9f1132c9e
Maksym Kupriichuk <30961944+kprm@users.noreply.github.com>
2020-05-12 18:00:09 +0300

    update sortMediaQueries

--- config/prod.cfg.js
+++ config/prod.cfg.js
@@ -251,27 +251,3 @@ module.exports = merge(baseConfig, {
     ]
   }
 });
-
-function isMax(mq) {
-  return /max-width/.test(mq);
-}
-
-function isMin(mq) {
-  return /min-width/.test(mq);
-}
-
-function sortMediaQueries(a, b) {
-  A = a.replace(/\D/g, '');
-  B = b.replace(/\D/g, '');
-
-  if (isMax(a) && isMax(b)) {
-    return B - A;
-  } else if (isMin(a) && isMin(b)) {
-    return A - B;
-  } else if (isMax(a) && isMin(b)) {
-    return 1;
-  } else if (isMin(a) && isMax(b)) {
-    return -1;
-  }
-  return 1;
-}
