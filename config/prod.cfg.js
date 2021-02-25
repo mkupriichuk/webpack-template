40e389ddffc8b8f87f56bcc844ff72fda88c330a
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-02-25 18:06:29 +0200

    moved css-mqpacker from postcss to node

--- config/prod.cfg.js
+++ config/prod.cfg.js
@@ -130,23 +130,6 @@ module.exports = merge(baseConfig, {
 	},
 });
 
-function sortMediaQueries(a, b) {
-	let A = a.replace(/\D/g, "");
-	let B = b.replace(/\D/g, "");
-
-	if (/max-width/.test(a) && /max-width/.test(b)) {
-		return B - A;
-	} else if (/min-width/.test(a) && /min-width/.test(b)) {
-		return A - B;
-	} else if (/max-width/.test(a) && /min-width/.test(b)) {
-		return 1;
-	} else if (/min-width/.test(a) && /max-width/.test(b)) {
-		return -1;
-	}
-
-	return 1;
-}
-
 function styleLoaders(preProcessor) {
 	const loaders = [
 		{
@@ -171,13 +154,7 @@ function styleLoaders(preProcessor) {
 							{
 								grid: true,
 							},
-						],
-						[
-							"css-mqpacker",
-							{
-								sort: sortMediaQueries,
-							},
-						],
+						]
 					],
 				},
 			},
