ba5a31d2b258ff76a410447519d2e81cdea68de6
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-02-25 18:09:40 +0200

    moved css-mqpacker from postcss to node

--- config/prod.cfg.js
+++ config/prod.cfg.js
@@ -130,28 +130,7 @@ module.exports = merge(baseConfig, {
 	},
 });
 
-<<<<<<< HEAD
-function sortMediaQueries(a, b) {
-	let A = a.replace(/\D/g, "");
-	let B = b.replace(/\D/g, "");
-
-	if (/max-width/.test(a) && /max-width/.test(b)) {
-		return B - A;
-	} else if (/min-width/.test(a) && /min-width/.test(b)) {
-		return A - B;
-	} else if (/max-width/.test(a) && /min-width/.test(b)) {
-		return 1;
-	} else if (/min-width/.test(a) && /max-width/.test(b)) {
-		return -1;
-	}
-
-	return 1;
-}
-
-function styleLoaders() {
-=======
 function styleLoaders(preProcessor) {
->>>>>>> 40e389d (moved css-mqpacker from postcss to node)
 	const loaders = [
 		{
 			loader: MiniCssExtractPlugin.loader,
