7549545563fc388b283b8f4f8479d99168d7e5e4
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-03-08 14:30:23 +0200

    update svgo and exclude options

--- config/paths.js
+++ config/paths.js
@@ -3,5 +3,6 @@ const { resolve } = require('path');
 module.exports = {
   src: resolve(__dirname, '../src'),
   static: resolve(__dirname, '../static'),
+  nodeModules: resolve(__dirname, '../node_modules'),
   dist: resolve(__dirname, '../dist')
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import '/app.css'
-import samoyed from 'images/samoyed.jpg'
-import twitter from 'icons/twitter.svg'
+import "/app.css";
+import samoyed from "images/samoyed.jpg";
+import twitter from "icons/twitter.svg";
 
 const style = {
   backgroundImage: `url(${samoyed})`,
-  minHeight: '70vh',
-  color: 'pink',
-  textAlign: 'center',
-  fontSize: '300px'
-}
+  minHeight: "70vh",
+  color: "pink",
+  textAlign: "center",
+  fontSize: "100px",
+};
 
 const App = () => {
   return (
     <>
-    <div className="container" style={style}>
-      <h1 style={style}>Hello</h1>
-      <div className="hero">
-        <img src={twitter} alt="twitter logo"/>
+      <div className="container" style={style}>
+        <h1>Hello</h1>
+        <div className="hero">
+          <img src={twitter} alt="twitter logo" />
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
