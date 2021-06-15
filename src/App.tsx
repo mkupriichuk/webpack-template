1ba99195dc1b6d0a917426dd3bd4848865146608
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-06-15 14:56:38 +0300

    configurate eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import './styles/app.scss';
-import twitter from 'icons/twitter.svg'
-import samoyed from 'images/samoyed.jpg'
-import React from 'react';
-import { Button } from './components';
+import "./styles/app.scss";
+import Twitter from "icons/twitter.svg";
+import samoyed from "images/samoyed.jpg";
+import React from "react";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-    <div className="container">
-      <h1>Hello</h1>
-      <div className="hero">
-        <img src={twitter} alt=""/>
-        <img src={samoyed} alt=""/>
-      </div>
-			<Button type="big">button</Button>
-    </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div className="container">
+				<h1>Hello</h1>
+				<div className="hero">
+					<img src={samoyed} alt="" />
+					<Twitter />
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default App
+export default App;
