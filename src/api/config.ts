290fdab2bf458de45504d40756042951dd06b9ea
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2021-10-19 17:46:45 +0300

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 
-axios.defaults.baseURL = process.env.API_URL || 'https://jsonplaceholder.typicode.com/todos';
+axios.defaults.baseURL = process.env.API_URL || 'https://jsonplaceholder.typicode.com/posts';
 
 axios.interceptors.request.use(
 	(config) => {
