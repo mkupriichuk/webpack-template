8909572c4cd40eee811c7fa08afb1c41cfff529d
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-10-20 14:55:08 +0300

    set observableRequiresReaction: true in mobx

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,6 +1,7 @@
 import FooStore, { FooStoreType } from "./fooStore";
 import BarStore, { BarStoreType } from "./barStore";
 import { createContext } from "react";
+import { configure } from "mobx";
 export class RootStore {
 	fooStore: FooStoreType;
 	barStore: BarStoreType;
@@ -9,6 +10,11 @@ export class RootStore {
 		this.barStore = new BarStore(this);
 	}
 }
+
+/**
+ Warns about any unobserved observable access. Use this if you want to check whether you are using observables without a "MobX context". This is a great way to find any missing observer wrappers, for example in React components. But it will find missing actions as well.
+ */
+configure({ observableRequiresReaction: true });
 
 export type RootStoreType = RootStore;
 
