8c79b6b312ab23d8e0080a10ee4354e365e1187f
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2021-10-21 18:44:51 +0300

    Update rootStore.ts

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -18,7 +18,9 @@ configure({ observableRequiresReaction: true });
 
 export type RootStoreType = RootStore;
 
-export const RootStoreContext = createContext(new RootStore());
+export const store = new RootStore()
+
+export const RootStoreContext = createContext(store);
 
 // export const RootStoreProvider = ({children}) => {
 // 	return <RootStoreContext.Provider value={new RootStore()}>{children}</RootStoreContext.Provider>
