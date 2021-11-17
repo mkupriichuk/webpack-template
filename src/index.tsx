685cc723c347514e05a15acacd50ec7978ea568f
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-11-17 20:25:04 +0200

    add provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,18 @@
-import {render} from 'react-dom';
-import App from './App';
-render(<App />, document.getElementById('root'));
+import { render } from "react-dom";
+import App from "./App";
+import { RootStoreContext, appStore } from "./store/rootStore";
+
+const RootStoreProvider = ({ children }) => {
+	return (
+		<RootStoreContext.Provider value={appStore}>
+			{children}
+		</RootStoreContext.Provider>
+	);
+};
+
+render(
+	<RootStoreProvider>
+		<App />
+	</RootStoreProvider>,
+	document.getElementById("root")
+);
--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -18,10 +18,7 @@ configure({ observableRequiresReaction: true });
 
 export type RootStoreType = RootStore;
 
-export const store = new RootStore();
+export const appStore = new RootStore();
 
-export const RootStoreContext = createContext(store);
+export const RootStoreContext = createContext(appStore);
 
-// export const RootStoreProvider = ({children}) => {
-// 	return <RootStoreContext.Provider value={new RootStore()}>{children}</RootStoreContext.Provider>
-// }
