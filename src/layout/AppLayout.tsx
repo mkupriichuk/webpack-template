3b3f672fed3d5bcb9d1d170c3180ae317d7ffa75
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2021-11-22 14:24:53 +0200

    Update AppLayout.tsx

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -2,7 +2,7 @@ import style from "./layout.module.scss";
 
 const AppLayout: React.FC = ({ children }) => {
 	return <div className={style.app}>
-		<header className={style.header}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quam quas, assumenda ab veritatis saepe deleniti necessitatibus itaque fuga earum iusto! Nisi et quibusdam molestias! A quibusdam consequuntur architecto temporibus repellat maxime itaque laborum porro est magni praesentium maiores tempore fugit et repudiandae eveniet, quia culpa doloribus quisquam accusantium quis?</header>
+		<header className={style.header}>header</header>
 		<main className={style.main}>
 			{children}
 		</main>
