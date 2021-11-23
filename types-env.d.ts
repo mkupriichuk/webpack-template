dde2108bef2f863ac6cbdf220696cbc7e175111a
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-11-23 19:11:38 +0200

    update webpack

--- types-env.d.ts
+++ types-env.d.ts
@@ -34,8 +34,8 @@ declare module "*.webp" {
 }
 
 declare module "*.svg" {
-  const content: any;
-  export default content;
+	const content: any;
+	export default content;
 }
 
 declare module "*.module.scss" {
