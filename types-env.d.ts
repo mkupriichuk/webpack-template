e41238d35ad959f4e1c1ad681995fd02490ec1c8
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2021-11-23 17:09:39 +0200

    Update types-env.d.ts

--- types-env.d.ts
+++ types-env.d.ts
@@ -34,14 +34,9 @@ declare module "*.webp" {
 }
 
 declare module "*.svg" {
-	import * as React from "react";
-
-	export const ReactComponent: React.FunctionComponent<
-		React.SVGProps<SVGSVGElement> & { title?: string }
-	>;
-
-	const src: string;
-	export default src;
+  import { ReactElement, SVGProps } from "react";
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
 }
 
 declare module "*.module.scss" {
