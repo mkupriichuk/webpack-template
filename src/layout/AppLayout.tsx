469648e539a4b8c90de075a0ec26a46f141d02b1
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-11-24 20:17:24 +0200

    update layout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,13 +1,16 @@
 import style from "./layout.module.scss";
 
 const AppLayout: React.FC = ({ children }) => {
-	return <div className={style.app}>
-		<header className={style.header}>header</header>
-		<main className={style.main}>
-			{children}
-		</main>
-		<footer className={style.footer}>footer</footer>
-	</div>;
+	return (
+		<div className={style.app}>
+			<header className={style.header}>header</header>
+			<div className={style.main}>
+				<div className={style.sidebar}>sidebar</div>
+				<div className={style.content}>{children}</div>
+			</div>
+			{/* <footer className={style.footer}>footer</footer> */}
+		</div>
+	);
 };
 
 export default AppLayout;
