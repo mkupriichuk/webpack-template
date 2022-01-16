ee113efcff438f2675da17b0accc4cff8c5a0dd7
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2022-01-16 20:50:15 +0200

    update svg handler

--- types-env.d.ts
+++ types-env.d.ts
@@ -28,21 +28,30 @@ declare module "*.png" {
 	export default src;
 }
 
+declare module "*.webp" {
+	const src: string;
+	export default src;
+}
+
+
+// declare svg types for @svgr
+declare module "*.svg" {
+	const content: React.FunctionComponent<React.SVGAttributes<SVGAElement>>;
+	export default content;
+}
+
+// declare svg types for file-loader
+declare module "*.svg?url" {
+	const path: string;
+	export default path;
+}
+
 declare module "*.mp3" {
 	const src: string;
 	export default src;
 }
 
 declare module "*.mp4" {
-	const src: string;
-	export default src;
-}
-
-declare module "*.svg" {
-	const content: any;
-	export default content;
-}
-declare module "*.webp" {
 	const src: string;
 	export default src;
 }
