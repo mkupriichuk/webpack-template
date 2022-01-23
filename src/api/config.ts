28d1538f1457f92d411f461b2a1189a6f680283d
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2022-01-23 21:32:22 +0200

    update api cfg

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,27 +1,92 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-
-axios.interceptors.request.use(
-	(config) => {
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
+export class Requests {
+	private instance: AxiosInstance;
+	constructor(instance: AxiosInstance) {
+		this.instance = instance;
 	}
-);
+	get(url: string, options?: AxiosRequestConfig): Promise<any> {
+		return this.instance.get(url, options).then(this.responseBody);
+	}
+	post(
+		url: string,
+		body: Record<string, unknown>,
+		options?: AxiosRequestConfig
+	): Promise<any> {
+		return this.instance.post(url, body, options).then(this.responseBody);
+	}
+	patch(
+		url: string,
+		body: Record<string, unknown>,
+		options?: AxiosRequestConfig
+	): Promise<any> {
+		return this.instance.patch(url, body, options).then(this.responseBody);
+	}
+	put(
+		url: string,
+		body: Record<string, unknown>,
+		options?: AxiosRequestConfig
+	): Promise<any> {
+		return this.instance.put(url, body, options).then(this.responseBody);
+	}
+	del(url: string, options?: AxiosRequestConfig): Promise<any> {
+		return this.instance.delete(url, options).then(this.responseBody);
+	}
+	private responseBody(response: AxiosResponse) {
+		return response.data;
+	}
+}
 
-axios.interceptors.response.use(undefined, (error) => {
-	throw error.response;
-});
+// axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-const responseBody = (response: AxiosResponse) => response.data;
+// axios.interceptors.request.use(
+// 	(config) => {
+// 		return config;
+// 	},
+// 	(error) => {
+// 		return Promise.reject(error);
+// 	}
+// );
 
-export const requests = {
-	get: (url: string, options?: AxiosRequestConfig): Promise<any> => axios.get(url, options).then(responseBody),
-	post: (url: string, body: Record<string, unknown>, options?: AxiosRequestConfig): Promise<any> =>
-		axios.post(url, body, options).then(responseBody),
-	put: (url: string, body: Record<string, unknown>, options?: AxiosRequestConfig): Promise<any> =>
-		axios.put(url, body, options).then(responseBody),
-	del: (url: string, options?: AxiosRequestConfig): Promise<any> => axios.delete(url, options).then(responseBody),
-};
+// axios.interceptors.response.use(undefined, (error) => {
+// 	throw error.response;
+// });
+// // axios.defaults.baseURL = process.env.API_URL;
+// authInstance.interceptors.request.use(
+// 	(config) => {
+// 		const token = window.localStorage.getItem("token");
+// 		if (token) config.headers!.Authorization = `Bearer ${token}`;
+// 		return config;
+// 	},
+// 	(error) => {
+// 		return Promise.reject(error);
+// 	}
+// );
+
+// authInstance.interceptors.response.use(
+// 	(config) => {
+// 		return config;
+// 	},
+// 	async (error) => {
+// 		const originalRequest = error.config;
+// 		if (
+// 			error.response?.status == 401 && !originalRequest._retry
+// 		) {
+// 			originalRequest._retry = true;
+// 			try {
+// 				const response = await axios.get<IUser>(`${process.env.AUTH_API_URL}/refresh`, {
+// 					withCredentials: true,
+// 				});
+// 				localStorage.setItem("token", response.data.token);
+// 				return authInstance.request(originalRequest);
+// 			} catch (_error) {
+// 				if (_error.response && _error.response.data) {
+// 					return Promise.reject(_error.response.data);
+// 				}
+
+// 				return Promise.reject(_error);
+// 			}
+// 		}
+// 		throw error;
+// 	}
+// );
