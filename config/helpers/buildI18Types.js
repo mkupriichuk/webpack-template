a4735e1020df4c1aa93c02aaf774a6bd9dfd7a5a
Maksym Kupriichuk <30961944+mkupriichuk@users.noreply.github.com>
2022-01-27 17:06:46 +0200

    add buildI18Types.js

---
+++ config/helpers/buildI18Types.js
@@ -0,0 +1,45 @@
+const PATH = require('../paths.js');
+const fs = require('fs');
+const chalk = require('chalk');
+
+const localePath = PATH.public + '/locales/en/translation.json';
+
+try {
+const _locale = fs.readFileSync(localePath, 'utf-8');
+const typedHookPath = PATH.src + '/hooks/useTypedTranslation';
+const outputTypeFile = typedHookPath + '/types.ts';
+const translationTypeName = 'TranslationKeysType'
+
+if (_locale && fs.existsSync(typedHookPath)) {
+	const locale = JSON.parse(_locale);
+	function buildI18Types(obj, path = '') {
+		const res = {};
+		for (const key in obj) {
+			if (typeof obj[key] == 'string' && !key.includes('_plural')) {
+				res[[...path, key].join('.')] = obj[key];
+			} else if (typeof obj[key] == 'object') {
+				Object.assign(res, buildI18Types(obj[key], [...path, key]));
+			}
+		}
+		return res;
+	}
+
+	const literals = Object.keys(buildI18Types(locale));
+	const result = `export type ${translationTypeName} = ${literals
+		.map((el) => "'" + el + "'")
+		.join(' | ')};`;
+	fs.writeFile(outputTypeFile, result, (err) => {
+		if (err) {
+			console.log('Something goes wrong:(');
+		} else {
+			console.log(`Types file was created. See ${chalk.green(outputTypeFile)}`);
+		}
+	});
+}
+} catch (error) {
+	if(error.code = 'ENOEND') {
+		console.log(`Failed to read file ${chalk.green(localePath)}`);
+	} else {
+		console.log(error);
+	}
+}
